0848b40c71753267341049d7a03544f0619ff1ea
W1ndys <w1ndys@outlook.com>
2024-12-30 18:10:44 +0800

    添加项目初始文件，包括依赖、Docker配置和基本功能实现

---
+++ get_exam_info.py
@@ -0,0 +1,18 @@
+
+
+
+# 读取账号密码
+def read_user_credentials():
+    with open("user_credentials_cleaned.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    return lines
+
+# 分离每组账号密码
+
+
+
+
+
+
+if __name__ == "__main__":
+    
